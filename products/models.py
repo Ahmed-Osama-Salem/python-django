adf96ff3ddb8e6f00d1e12ae0820b7e60aa72f52
Ahmed-Osama-Salem <ahmedosamaweb@gmail.com>
2023-03-31 23:11:56 +0200

    create product models,view,temp

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Create your table for products by create class.
+
+class Products(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=7,decimal_places=2)
+    discription = models.TextField()
+    image= models.ImageField(upload_to= "images/%y/%m/%d")
+    active= models.BooleanField(default=False)
+
+    
--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Products
 # Create your views here.
 def products(req):
-    return render(req,"products/products.html")
+    return render(req,"products/products.html", {"productsDB": Products.objects.all()})
